3da25f58e5c3e0ebdda5fdd40a78e98383069f1e
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-03-18 18:02:59 -0700

    initial implementation of node project

---
+++ server.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+require('./db');
+const bcrypt = require("bcrypt")
+const axios = require('axios')
+const session = require('express-session');
+const dotenv = require("dotenv")
+dotenv.config();
+
+// require model and load it; clear DB when server is rebooted
+const User = require('./models/user.js')
+app.use('/', express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 10800000 
+    }
+}));
+
+function isLoggedIn(req, res, next) {
+    if (req.session.isLoggedIn) {
+        return next();
+    } else {
+        return res.redirect('/login');
+    }
+}
+
+function isLoggedOut(req, res, next) {
+    if (!req.session.isLoggedIn) {
+        return next();
+    } else {
+        return res.redirect('/');
+    }
+}
+
+app.get('/', isLoggedIn, (req, res) => {
+    res.sendFile(__dirname + '/html/index.html')
+})
+
+app.get('/account', isLoggedIn, (req, res) => {
+    res.sendFile(__dirname + '/html/account.html')
+})
+
+app.get('/login', isLoggedOut, (req, res) => {
+    res.sendFile(__dirname + '/html/login.html')
+})
+
+app.get('/register', isLoggedOut, (req, res) => {
+    res.sendFile(__dirname + '/html/register.html')
+})
+
+app.get('/logout', (req, res)=>{
+    req.session.destroy();
+    return res.redirect('/login')
+} )
+
+app.post('/register', isLoggedOut, async (req, res) => {
+    console.log(req.body)
+    const {name, email, password} = req.body
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const userWithHashedPassword = { name:name, email:email, password: hashedPassword }
+    const user = await User.create(userWithHashedPassword)
+    req.session.user = user;
+    req.session.isLoggedIn = true;
+    return res.redirect('/')
+})
+
+app.post('/login', isLoggedOut, async (req, res) => {
+    try {
+        const {email, password} = req.body
+        let user = await User.findOne({email:email})
+        if (!user)
+            return res.json('No user') 
+        comp = bcrypt.compare(password, user.password)
+        if (!comp)
+            return res.json('WrongPass')
+        req.session.user = user;
+        req.session.isLoggedIn = true;
+        return res.redirect('/')
+    } catch (err){
+        res.json(err)
+    }
+})
+
+app.get('*', (req, res) => {
+  res.status(404).json({
+    msg: 'Improper route. Check API docs plz.'
+  })
+})
+
+// Start the server
+app.listen(process.env.PORT, () => {
+  console.log("Server started on port " + process.env.PORT);
+});
